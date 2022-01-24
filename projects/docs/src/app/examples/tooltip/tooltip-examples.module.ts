a5be86ab7525e002ea575e292e86284c51a3c835
Valentin Gavran <valentin.gavran@icloud.com>
2022-01-24 20:27:48 +0100

    Replace native tooltip example buttons with Anglify buttons

--- projects/docs/src/app/examples/tooltip/tooltip-examples.module.ts
+++ projects/docs/src/app/examples/tooltip/tooltip-examples.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LongTextComponent } from './long-text/long-text.component';
 import { TooltipModule } from '../../../../../anglify/src/modules/tooltip/tooltip.module';
 import { DifferentPositionsComponent } from './different-positions/different-positions.component';
+import { ButtonModule } from '../../../../../anglify/src/modules/button/button.module';
 
 @NgModule({
-  imports: [CommonModule, TooltipModule],
+  imports: [CommonModule, TooltipModule, ButtonModule],
   declarations: [LongTextComponent, DifferentPositionsComponent],
 })
 export class TooltipExamplesModule {}
