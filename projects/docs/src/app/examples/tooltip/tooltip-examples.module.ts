b1e16be879689cc0c70e94708befea0f110f9fe9
Valentin Gavran <valentin.gavran@icloud.com>
2022-02-04 13:36:53 +0100

    Add possibility to open and close tooltips manually
    Closes #17

--- projects/docs/src/app/examples/tooltip/tooltip-examples.module.ts
+++ projects/docs/src/app/examples/tooltip/tooltip-examples.module.ts
@@ -6,9 +6,10 @@ import { DifferentPositionsComponent } from './different-positions/different-pos
 import { ButtonModule } from '../../../../../anglify/src/modules/button/button.module';
 import CustomContentComponent from './custom-content/custom-content.component';
 import { IconModule } from '../../../../../anglify/src/modules/icon/icon.module';
+import { ShowHideManuallyComponent } from './show-hide-manually/show-hide-manually.component';
 
 @NgModule({
-  imports: [CommonModule, TooltipModule, ButtonModule, IconModule],
-  declarations: [LongTextComponent, DifferentPositionsComponent, CustomContentComponent],
+  imports: [CommonModule, TooltipModule, ButtonModule, IconModule, ButtonModule, TooltipModule],
+  declarations: [LongTextComponent, DifferentPositionsComponent, CustomContentComponent, ShowHideManuallyComponent],
 })
 export class TooltipExamplesModule {}
