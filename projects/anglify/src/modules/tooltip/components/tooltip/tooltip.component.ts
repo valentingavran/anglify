442ade506bcc35b39ece83534e15d483da7c9939
Valentin Gavran <valentin.gavran@icloud.com>
2022-03-15 15:30:44 +0100

    refactor(tooltip): Extract tooltip positioning into own service
    To make Anglify more modular and reusable, positioning is extracted into a service. This allows other components to use it as well
    
    Close #57

--- projects/anglify/src/modules/tooltip/components/tooltip/tooltip.component.ts
+++ projects/anglify/src/modules/tooltip/components/tooltip/tooltip.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, Inject, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import { TooltipPosition } from '../../tooltip.interface';
-import { observeOnResize } from '../../../../utils/functions';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { fromEvent, merge } from 'rxjs';
-import { DEFAULT_TOOLTIP_SETTINGS } from '../../tooltip-settings.token';
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { Position } from '../../../../composables/position/position.interface';
+import { MENUABLE } from '../../../../composables/position/position.provider';
+import { PositionService } from '../../../../composables/position/position.service';
 
 @UntilDestroy()
 @Component({
@@ -12,52 +11,24 @@ import { DEFAULT_TOOLTIP_SETTINGS } from '../../tooltip-settings.token';
   styleUrls: ['./tooltip.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
+  providers: [MENUABLE],
 })
-export class TooltipComponent implements OnInit {
-  @Input() public position: TooltipPosition = DEFAULT_TOOLTIP_SETTINGS.position;
-  @Input() public offset = DEFAULT_TOOLTIP_SETTINGS.defaultOffset;
+export class TooltipComponent {
+  @Input()
+  public set position(value: Position) {
+    this.positionService.position = value;
+  }
+
+  @Input()
+  public set offset(value: number) {
+    this.positionService.offset = value;
+  }
 
   @HostBinding('class')
   @Input()
   public contentClass?: string;
 
-  public constructor(@Inject('tooltipConfig') private readonly config: any, private readonly element: ElementRef) {
+  public constructor(private readonly positionService: PositionService, private readonly element: ElementRef) {
     this.element.nativeElement.classList.add('anglify-tooltip');
-
-    merge(observeOnResize(this.element.nativeElement as HTMLElement), fromEvent(window, 'scroll', { capture: true }))
-      .pipe(untilDestroyed(this))
-      .subscribe(() => this.updatePosition()); // Used to center dynamic content inside tooltip
-  }
-
-  public ngOnInit(): void {
-    this.updatePosition();
-  }
-
-  private updatePosition(): TooltipComponent {
-    const hostPos = this.config.host.getBoundingClientRect();
-    const tooltipPos = this.element.nativeElement.getBoundingClientRect();
-
-    let top;
-    let left;
-
-    if (this.position === 'top') {
-      top = hostPos.top - tooltipPos.height - this.offset;
-      left = Math.max(Number(hostPos.left) + (hostPos.width - tooltipPos.width) / 2, this.offset);
-    } else if (this.position === 'bottom') {
-      top = Number(hostPos.bottom) + this.offset;
-      left = Math.max(Number(hostPos.left) + (hostPos.width - tooltipPos.width) / 2, this.offset);
-    } else {
-      top = Number(hostPos.top) + (hostPos.height - tooltipPos.height) / 2;
-      if (this.position === 'left') {
-        left = Math.max(hostPos.left - tooltipPos.width - this.offset, this.offset);
-      } else {
-        left = Math.min(Number(hostPos.right) + this.offset, window.innerWidth - tooltipPos.width - this.offset);
-      }
-    }
-
-    this.element.nativeElement.style.top = `${top}px`;
-    this.element.nativeElement.style.left = `${left}px`;
-
-    return this;
   }
 }
