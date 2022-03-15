db4034d87af390cd71ae03ee9c59743bea6b107c
Valentin Gavran <valentin.gavran@icloud.com>
2022-03-15 15:30:44 +0100

    feature(list): Implement list component
    Closes #59

---
+++ projects/anglify/src/modules/list/components/list-item/list-item.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, ContentChild, Input } from '@angular/core';
+import { BooleanLike } from '../../../../utils/interfaces';
+import { AppendDirective } from '../../directives/append/append.directive';
+import { PrependDirective } from '../../directives/prepend/prepend.directive';
+
+@Component({
+  selector: 'anglify-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ListItemComponent {
+  @ContentChild(AppendDirective) public readonly appendDirective?: AppendDirective;
+  @ContentChild(PrependDirective) public readonly prependDirective?: PrependDirective;
+
+  @Input() public dense: BooleanLike = false;
+  @Input() public disabled: BooleanLike = false;
+}
--- projects/anglify/src/modules/overlay/overlay.directive.ts
+++ projects/anglify/src/modules/overlay/overlay.directive.ts
@@ -7,7 +7,7 @@ import { isBooleanLikeTrue } from '../../utils/functions';
 
 @UntilDestroy(this)
 @Directive({
-  selector: '[anglifyOverlay], [anglifyButton], anglify-stepper-header',
+  selector: '[anglifyOverlay], [anglifyButton], anglify-stepper-header, anglify-list-item[click]',
 })
 export class OverlayDirective {
   @Input()
