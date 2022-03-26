bee31661640998c13ba349761c0b54d9409e2bab
Valentin Gavran <valentin.gavran@icloud.com>
2022-03-26 09:53:00 +0100

    docs: Add installation guide

--- projects/docs/src/app/app-routing.module.ts
+++ projects/docs/src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IconPageComponent } from './pages/component-pages/icon-page/icon-page.component';
-import { OverlayPageComponent } from './pages/component-pages/overlay-page/overlay-page.component';
-import { StepperComponent } from './pages/component-pages/stepper/stepper.component';
-import { FormFieldPageComponent } from './pages/component-pages/form-field-page/form-field-page.component';
-import { TooltipPageComponent } from './pages/component-pages/tooltip-page/tooltip-page.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ButtonPageComponent } from './pages/component-pages/button-page/button-page.component';
+import { CardPageComponent } from './pages/component-pages/card-page/card-page.component';
+import { FormFieldPageComponent } from './pages/component-pages/form-field-page/form-field-page.component';
+import { IconPageComponent } from './pages/component-pages/icon-page/icon-page.component';
 import { ListPageComponent } from './pages/component-pages/list-page/list-page.component';
 import { MenuPageComponent } from './pages/component-pages/menu-page/menu-page.component';
-import { CardPageComponent } from './pages/component-pages/card-page/card-page.component';
+import { OverlayPageComponent } from './pages/component-pages/overlay-page/overlay-page.component';
+import { StepperComponent } from './pages/component-pages/stepper/stepper.component';
+import { TooltipPageComponent } from './pages/component-pages/tooltip-page/tooltip-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InstallationComponent } from './pages/installation/installation.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'getting-started/installation',
+    component: InstallationComponent,
   },
   {
     path: 'components/button',
--- projects/docs/src/app/app.module.ts
+++ projects/docs/src/app/app.module.ts
@@ -3,35 +3,36 @@ import { NgModule, SecurityContext } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HIGHLIGHT_OPTIONS, HighlightModule } from 'ngx-highlightjs';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 import { MarkdownModule } from 'ngx-markdown';
-import { IconModule } from '../../../anglify/src/modules/icon/icon.module';
+import { ButtonModule } from '../../../anglify/src/modules/button/button.module';
+import { CardModule } from '../../../anglify/src/modules/card/card.module';
 import { FormFieldModule } from '../../../anglify/src/modules/form-field/form-field.module';
+import { IconModule } from '../../../anglify/src/modules/icon/icon.module';
+import { ListModule } from '../../../anglify/src/modules/list/list.module';
+import { MenuModule } from '../../../anglify/src/modules/menu/menu.module';
+import { OverlayModule } from '../../../anglify/src/modules/overlay/overlay.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CodeExampleComponent } from './components/code-example/code-example.component';
 import { StylingTableComponent } from './components/styling-table/styling-table.component';
+import { ButtonExamplesModule } from './examples/button/button-examples.module';
+import { FormFieldExamplesModule } from './examples/form-field/form-field-examples.module';
 import { IconExamplesModule } from './examples/icon/icon-examples.module';
 import { OverlayExamplesModule } from './examples/overlay/overlay-examples.module';
 import { StepperExamplesModule } from './examples/stepper/stepper-examples.module';
-import { FormFieldExamplesModule } from './examples/form-field/form-field-examples.module';
 import { TooltipExamplesModule } from './examples/tooltip/tooltip-examples.module';
+import { ButtonPageComponent } from './pages/component-pages/button-page/button-page.component';
+import { CardPageComponent } from './pages/component-pages/card-page/card-page.component';
+import { FormFieldPageComponent } from './pages/component-pages/form-field-page/form-field-page.component';
 import { IconPageComponent } from './pages/component-pages/icon-page/icon-page.component';
+import { ListPageComponent } from './pages/component-pages/list-page/list-page.component';
+import { MenuPageComponent } from './pages/component-pages/menu-page/menu-page.component';
 import { OverlayPageComponent } from './pages/component-pages/overlay-page/overlay-page.component';
 import { StepperComponent } from './pages/component-pages/stepper/stepper.component';
-import { FormFieldPageComponent } from './pages/component-pages/form-field-page/form-field-page.component';
 import { TooltipPageComponent } from './pages/component-pages/tooltip-page/tooltip-page.component';
 import { HomeComponent } from './pages/home/home.component';
-import { OverlayModule } from '../../../anglify/src/modules/overlay/overlay.module';
-import { ButtonPageComponent } from './pages/component-pages/button-page/button-page.component';
-import { ButtonExamplesModule } from './examples/button/button-examples.module';
-import { ButtonModule } from '../../../anglify/src/modules/button/button.module';
-import { ListPageComponent } from './pages/component-pages/list-page/list-page.component';
-import { ListModule } from '../../../anglify/src/modules/list/list.module';
-import { MenuModule } from '../../../anglify/src/modules/menu/menu.module';
-import { MenuPageComponent } from './pages/component-pages/menu-page/menu-page.component';
-import { CardPageComponent } from './pages/component-pages/card-page/card-page.component';
-import { CardModule } from '../../../anglify/src/modules/card/card.module';
+import { InstallationComponent } from './pages/installation/installation.component';
 
 @NgModule({
   declarations: [
@@ -48,6 +49,7 @@ import { CardModule } from '../../../anglify/src/modules/card/card.module';
     ListPageComponent,
     MenuPageComponent,
     CardPageComponent,
+    InstallationComponent,
   ],
   imports: [
     BrowserModule,
@@ -81,6 +83,8 @@ import { CardModule } from '../../../anglify/src/modules/card/card.module';
           typescript: () => import('highlight.js/lib/languages/typescript'),
           scss: () => import('highlight.js/lib/languages/scss'),
           xml: () => import('highlight.js/lib/languages/xml'),
+          shell: () => import('highlight.js/lib/languages/shell'),
+          bash: () => import('highlight.js/lib/languages/bash'),
         },
       },
     },
