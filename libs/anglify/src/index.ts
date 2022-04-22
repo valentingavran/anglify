4c3f0ecb5fde03ca9be0a4ea24a64527faf2345a
Valentin Gavran <valentin.gavran@icloud.com>
2022-04-22 11:28:25 +0200

    Remove lib subfolder inside anglify library

--- libs/anglify/src/index.ts
+++ libs/anglify/src/index.ts
@@ -1,83 +1,83 @@
 // Button
-export * from './lib/modules/button/button.component';
-export * from './lib/modules/button/button.interface';
-export * from './lib/modules/button/button.module';
+export * from './modules/button/button.component';
+export * from './modules/button/button.interface';
+export * from './modules/button/button.module';
 // Card
-export * from './lib/modules/card/card-settings.token';
-export * from './lib/modules/card/card.component';
-export * from './lib/modules/card/card.interface';
-export * from './lib/modules/card/card.module';
+export * from './modules/card/card-settings.token';
+export * from './modules/card/card.component';
+export * from './modules/card/card.interface';
+export * from './modules/card/card.module';
 // Checkbox
-export * from './lib/modules/checkbox/interfaces/checkbox.interface';
-export * from './lib/modules/checkbox/interfaces/checkbox-icons.interface';
-export * from './lib/modules/checkbox/tokens/checkbox-icons.token';
-export * from './lib/modules/checkbox/tokens/checkbox.token';
-export * from './lib/modules/checkbox/checkbox.component';
-export * from './lib/modules/checkbox/functions/register-icons.function';
-export * from './lib/modules/checkbox/checkbox.module';
+export * from './modules/checkbox/interfaces/checkbox.interface';
+export * from './modules/checkbox/interfaces/checkbox-icons.interface';
+export * from './modules/checkbox/tokens/checkbox-icons.token';
+export * from './modules/checkbox/tokens/checkbox.token';
+export * from './modules/checkbox/checkbox.component';
+export * from './modules/checkbox/functions/register-icons.function';
+export * from './modules/checkbox/checkbox.module';
 // Dialog
-export * from './lib/modules/dialog/dialog-context.interface';
-export * from './lib/modules/dialog/dialog-options.interface';
-export * from './lib/modules/dialog/dialog.component';
-export * from './lib/modules/dialog/dialog.service';
-export * from './lib/modules/dialog/dialog.module';
+export * from './modules/dialog/dialog-context.interface';
+export * from './modules/dialog/dialog-options.interface';
+export * from './modules/dialog/dialog.component';
+export * from './modules/dialog/dialog.service';
+export * from './modules/dialog/dialog.module';
 // Form Field
-export * from './lib/modules/form-field/directives/input.directive';
-export * from './lib/modules/form-field/directives/label/label.directive';
-export * from './lib/modules/form-field/form-field-settings.token';
-export * from './lib/modules/form-field/form-field.component';
-export * from './lib/modules/form-field/form-field.interface';
-export * from './lib/modules/form-field/form-field.module';
+export * from './modules/form-field/directives/input.directive';
+export * from './modules/form-field/directives/label/label.directive';
+export * from './modules/form-field/form-field-settings.token';
+export * from './modules/form-field/form-field.component';
+export * from './modules/form-field/form-field.interface';
+export * from './modules/form-field/form-field.module';
 // Icon
-export * from './lib/modules/icon/icon-settings.token';
-export * from './lib/modules/icon/icon.component';
-export * from './lib/modules/icon/icon.interface';
-export * from './lib/modules/icon/icon.module';
+export * from './modules/icon/icon-settings.token';
+export * from './modules/icon/icon.component';
+export * from './modules/icon/icon.interface';
+export * from './modules/icon/icon.module';
 // List
-export * from './lib/modules/list/components/list-item-description/list-item-description.component';
-export * from './lib/modules/list/components/list-item-title/list-item-title.component';
-export * from './lib/modules/list/components/list-item/list-item.component';
-export * from './lib/modules/list/components/list/list.component';
-export * from './lib/modules/list/directives/append/append.directive';
-export * from './lib/modules/list/directives/prepend/prepend.directive';
-export * from './lib/modules/list/list.module';
+export * from './modules/list/components/list-item-description/list-item-description.component';
+export * from './modules/list/components/list-item-title/list-item-title.component';
+export * from './modules/list/components/list-item/list-item.component';
+export * from './modules/list/components/list/list.component';
+export * from './modules/list/directives/append/append.directive';
+export * from './modules/list/directives/prepend/prepend.directive';
+export * from './modules/list/list.module';
 // Menu
-export * from './lib/modules/menu/components/menu/menu.component';
-export * from './lib/modules/menu/menu-settings.token';
-export * from './lib/modules/menu/menu.directive';
-export * from './lib/modules/menu/menu.interface';
-export * from './lib/modules/menu/menu.module';
+export * from './modules/menu/components/menu/menu.component';
+export * from './modules/menu/menu-settings.token';
+export * from './modules/menu/menu.directive';
+export * from './modules/menu/menu.interface';
+export * from './modules/menu/menu.module';
 // Overlay
-export * from './lib/modules/overlay/overlay.directive';
-export * from './lib/modules/overlay/overlay.module';
+export * from './modules/overlay/overlay.directive';
+export * from './modules/overlay/overlay.module';
 // Progress Circular
-export * from './lib/modules/progress-circular/progress-circular-settings.token';
-export * from './lib/modules/progress-circular/progress-circular.component';
-export * from './lib/modules/progress-circular/progress-circular.interface';
-export * from './lib/modules/progress-circular/progress-circular.module';
+export * from './modules/progress-circular/progress-circular-settings.token';
+export * from './modules/progress-circular/progress-circular.component';
+export * from './modules/progress-circular/progress-circular.interface';
+export * from './modules/progress-circular/progress-circular.module';
 // Progress Linear
-export * from './lib/modules/progress-linear/progress-linear.component';
-export * from './lib/modules/progress-linear/progress-linear.module';
+export * from './modules/progress-linear/progress-linear.component';
+export * from './modules/progress-linear/progress-linear.module';
 // Radio Button
-export * from './lib/modules/radio-button/radio-button.component';
-export * from './lib/modules/radio-button/radio-button.interface';
-export * from './lib/modules/radio-button/radio-button.module';
+export * from './modules/radio-button/radio-button.component';
+export * from './modules/radio-button/radio-button.interface';
+export * from './modules/radio-button/radio-button.module';
 // Stepper
-export * from './lib/modules/stepper/components/stepper-header/stepper-header.component';
-export * from './lib/modules/stepper/components/stepper/stepper.component';
-export * from './lib/modules/stepper/directives/step/step.directive';
-export * from './lib/modules/stepper/directives/stepper-next/stepper-next.directive';
-export * from './lib/modules/stepper/directives/stepper-previous/stepper-previous.directive';
-export * from './lib/modules/stepper/directives/stepper-visited-icon/stepper-visited-icon.directive';
-export * from './lib/modules/stepper/services/stepper-settings/stepper-settings.service';
-export * from './lib/modules/stepper/services/stepper/stepper.service';
-export * from './lib/modules/stepper/stepper-testing.module';
-export * from './lib/modules/stepper/stepper.module';
+export * from './modules/stepper/components/stepper-header/stepper-header.component';
+export * from './modules/stepper/components/stepper/stepper.component';
+export * from './modules/stepper/directives/step/step.directive';
+export * from './modules/stepper/directives/stepper-next/stepper-next.directive';
+export * from './modules/stepper/directives/stepper-previous/stepper-previous.directive';
+export * from './modules/stepper/directives/stepper-visited-icon/stepper-visited-icon.directive';
+export * from './modules/stepper/services/stepper-settings/stepper-settings.service';
+export * from './modules/stepper/services/stepper/stepper.service';
+export * from './modules/stepper/stepper-testing.module';
+export * from './modules/stepper/stepper.module';
 // Table
-export * from './lib/modules/table/components/table/table.component';
-export * from './lib/modules/table/table.module';
+export * from './modules/table/components/table/table.component';
+export * from './modules/table/table.module';
 // Tooltip
-export * from './lib/modules/tooltip/tooltip-settings.token';
-export * from './lib/modules/tooltip/tooltip.directive';
-export * from './lib/modules/tooltip/tooltip.interface';
-export * from './lib/modules/tooltip/tooltip.module';
+export * from './modules/tooltip/tooltip-settings.token';
+export * from './modules/tooltip/tooltip.directive';
+export * from './modules/tooltip/tooltip.interface';
+export * from './modules/tooltip/tooltip.module';
