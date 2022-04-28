01ebceb7a602b7bce47e3eeac1f5f6ebf00d47c0
Valentin Gavran <valentin.gavran@icloud.com>
2022-04-28 15:51:47 +0200

    Remove unused token

--- libs/anglify/src/composables/ripple/ripple.token.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { RippleSettings } from './ripple.interface';
-
-export const RIPPLE_SETTINGS = new InjectionToken<RippleSettings>('Ripple settings');
