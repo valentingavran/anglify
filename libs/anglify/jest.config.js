6debdbc884f17364ef182c39695249d5e7419bad
Valentin Gavran <valentin.gavran@icloud.com>
2022-04-28 13:47:49 +0200

    Setup code coverage analysis

--- libs/anglify/jest.config.js
+++ libs/anglify/jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
+  coverageReporters: ['lcov'],
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
