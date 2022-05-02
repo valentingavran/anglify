7ec17935fb1b2e4adb3651cdef066deb6c68d4c4
Valentin Gavran <valentin.gavran@icloud.com>
2022-05-02 21:06:11 +0200

    feat: add observable HostBinding helper methods
    Closes #108

--- libs/anglify/src/utils/functions.ts
+++ libs/anglify/src/utils/functions.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { untilDestroyed } from '@ngneat/until-destroy';
+import { Observable, tap } from 'rxjs';
 import type { BooleanLike } from './interfaces';
 
 export function observeOnMutation(target: Node, config: MutationObserverInit | undefined): Observable<MutationRecord[]> {
@@ -31,4 +32,48 @@ export function isTouchDevice() {
 
 export function clamp(value: number, min: number, max: number): number {
   return value > max ? max : value < min ? min : value;
+}
+
+/**
+ * For the method to work, the __@UntilDestroy()__ decorator must be added to the component.
+ */
+export function bindClassToNativeElement(componentReference: any, data$: Observable<boolean>, element: HTMLElement, className: string) {
+  data$
+    .pipe(
+      untilDestroyed(componentReference),
+      tap(value => {
+        if (value) {
+          element.classList.add(className);
+        } else {
+          element.classList.remove(className);
+        }
+      })
+    )
+    .subscribe();
+}
+
+/**
+ * For the method to work, the __@UntilDestroy()__ decorator must be added to the component.
+ */
+export function bindStyleToNativeElement(
+  componentReference: any,
+  data$: Observable<boolean>,
+  element: HTMLElement,
+  styleName: string,
+  styleValue: string
+) {
+  data$
+    .pipe(
+      untilDestroyed(componentReference),
+      tap(value => {
+        if (value) {
+          // @ts-expect-error
+          element.style[styleName] = styleValue;
+        } else {
+          // @ts-expect-error
+          element.style[styleName] = null;
+        }
+      })
+    )
+    .subscribe();
 }
