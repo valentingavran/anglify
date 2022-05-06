633e4dd745bd9193f853a2c8c7994b048254ab05
Valentin Gavran <valentin.gavran@icloud.com>
2022-05-06 09:47:45 +0200

    refactor(ListItemGroup): store only active element count

--- libs/anglify/src/modules/list/components/list-item-group/list-item-group.component.ts
+++ libs/anglify/src/modules/list/components/list-item-group/list-item-group.component.ts
@@ -1,9 +1,8 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ContentChildren, forwardRef, Input, QueryList } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { diff, pull, toBoolean } from 'libs/anglify/src/utils/functions';
+import { toBoolean } from 'libs/anglify/src/utils/functions';
 import { BooleanLike } from 'libs/anglify/src/utils/interfaces';
-import { BehaviorSubject, map, pairwise, startWith, tap } from 'rxjs';
 import { ListItemComponent } from '../list-item/list-item.component';
 
 @UntilDestroy()
@@ -21,7 +20,7 @@ import { ListItemComponent } from '../list-item/list-item.component';
   ],
 })
 export class ListItemGroupComponent implements AfterViewInit, ControlValueAccessor {
-  @ContentChildren(ListItemComponent) private readonly items?: QueryList<ListItemComponent>;
+  @ContentChildren(ListItemComponent, { descendants: true }) private readonly items?: QueryList<ListItemComponent>;
 
   @Input() public mandatory: BooleanLike = false;
   @Input() public multiple: BooleanLike = false;
@@ -29,37 +28,27 @@ export class ListItemGroupComponent implements AfterViewInit, ControlValueAccess
   public onChange: (...args: any[]) => void = () => {};
   public onTouch: (...args: any[]) => void = () => {};
 
-  private readonly selectedIndices$ = new BehaviorSubject<number[]>([]);
-  private readonly selectedIndicesHandler = this.selectedIndices$.pipe(
-    startWith([]),
-    pairwise(),
-    map(([oldIndices, newIndices]) => diff(oldIndices, newIndices)),
-    tap(diff => {
-      const itemsToDeselect = this.items?.filter((_, index) => diff.removed.includes(index));
-      const itemsToSelect = this.items?.filter((_, index) => diff.added.includes(index));
-
-      itemsToDeselect?.forEach(item => (item.active = false));
-      itemsToSelect?.forEach(item => (item.active = true));
-    })
-  );
-
-  public constructor() {
-    this.selectedIndicesHandler.pipe(untilDestroyed(this)).subscribe();
-  }
+  private activeCount = 0;
 
   public ngAfterViewInit(): void {
-    this.items?.forEach((item, index) => this.createItemClickHandler(item, index));
+    this.items?.forEach(item => this.createItemClickHandler(item));
   }
 
   public writeValue(value: number | number[] | null): void {
-    let valueToBeSet: number[] = [];
+    let indicesToBeActive: number[] = [];
     if (Array.isArray(value)) {
-      valueToBeSet = value;
+      indicesToBeActive = value;
     } else if (value === null) {
     } else {
-      valueToBeSet.push(value);
+      indicesToBeActive.push(value);
     }
-    this.selectedIndices$.next(valueToBeSet);
+    this.items?.forEach((item, index) => {
+      if (indicesToBeActive.includes(index)) {
+        item.active = true;
+      } else {
+        item.active = false;
+      }
+    });
   }
 
   public registerOnChange(fn: (...args: any[]) => void): void {
@@ -70,26 +59,37 @@ export class ListItemGroupComponent implements AfterViewInit, ControlValueAccess
     this.onTouch = fn;
   }
 
-  private createItemClickHandler(item: ListItemComponent, index: number) {
-    item.onClick.pipe(untilDestroyed(this)).subscribe(() => this.handleItemClick(item, index));
+  private createItemClickHandler(item: ListItemComponent) {
+    item.onClick.pipe(untilDestroyed(this)).subscribe(() => this.handleItemClick(item));
   }
 
-  private handleItemClick(item: ListItemComponent, index: number) {
-    const otherItemsSelected = pull(this.selectedIndices$.value, index).length > 0;
+  private handleItemClick(item: ListItemComponent) {
+    let activeCount = this.activeCount;
+    if (item.active) activeCount -= 1;
+
+    const otherItemsSelected = activeCount > 0;
     if ((!otherItemsSelected && !toBoolean(item.active)) || (toBoolean(this.multiple) && !toBoolean(item.active))) {
-      this.addIndexToIndices(index);
+      this.selectItem(item);
     } else if ((!toBoolean(this.mandatory) && toBoolean(item.active)) || (otherItemsSelected && toBoolean(item.active))) {
-      this.removeIndexFromIndices(index);
+      this.deselectItem(item);
     } else if ((!toBoolean(this.multiple) && !otherItemsSelected) || !toBoolean(item.active)) {
-      this.selectedIndices$.next([index]);
+      this.deselectAll();
+      this.selectItem(item);
     }
   }
 
-  public addIndexToIndices(index: number) {
-    this.selectedIndices$.next([...this.selectedIndices$.value, index]);
+  public selectItem(item: ListItemComponent) {
+    item.active = true;
+    this.activeCount += 1;
   }
 
-  public removeIndexFromIndices(index: number) {
-    this.selectedIndices$.next(pull(this.selectedIndices$.value, index));
+  public deselectItem(item: ListItemComponent) {
+    item.active = false;
+    this.activeCount -= 1;
+  }
+
+  public deselectAll() {
+    this.items?.forEach(item => (item.active = false));
+    this.activeCount = 0;
   }
 }
