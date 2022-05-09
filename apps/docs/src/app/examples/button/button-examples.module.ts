3d645ec8348453b2b57aa1493c175325a1add7b2
iCrawl <buechler.noel@outlook.com>
2022-05-09 09:22:23 +0200

    refactor: material spec 2
    BREAKING CHANGE:
    
    - Removal of `elevated` button style.
    - - There is no reason for us to do this, we can already use the elevation utility for this.
    - Rename of `filled` button style to `contained` as per material spec.
    - Removal of `filled-tonal` button style.
    - - We can already handle changing the button color and text color within just a certain button really easily, no need to do this.
    - Removal of `sizing` property on buttons.
    - - We can handle changing the button size with CSS rather easily too, no need to do all out here and change it weirdly with props to have 5 fixed sizes.
    - Removal of `sizing` property on fab buttons.
    - - See above.

--- apps/docs/src/app/examples/button/button-examples.module.ts
+++ apps/docs/src/app/examples/button/button-examples.module.ts
@@ -5,11 +5,10 @@ import { AppearancesComponent } from './appearances/appearances.component';
 import { BlockComponent } from './block/block.component';
 import { DisabledComponent } from './disabled/disabled.component';
 import { FabComponent } from './fab/fab.component';
-import { SizingComponent } from './sizing/sizing.component';
 import { WithIconComponent } from './with-icon/with-icon.component';
 
 @NgModule({
-  declarations: [BlockComponent, WithIconComponent, SizingComponent, DisabledComponent, AppearancesComponent, FabComponent],
+  declarations: [BlockComponent, WithIconComponent, DisabledComponent, AppearancesComponent, FabComponent],
   imports: [CommonModule, ButtonModule, IconModule],
 })
 export class ButtonExamplesModule {}
