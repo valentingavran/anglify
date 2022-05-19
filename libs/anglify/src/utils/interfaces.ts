b3fe51c6d79e96671966b58c84a9202bc281a730
Lukas R <lukasrein97@gmail.com>
2022-05-19 09:44:13 +0200

    feat: list group collapsing

--- libs/anglify/src/utils/interfaces.ts
+++ libs/anglify/src/utils/interfaces.ts
@@ -1,3 +1,5 @@
 export type ComponentSize = 'x-small' | 'small' | 'regular' | 'large' | 'x-large';
 
 export type BooleanLike = boolean | 'true' | 'false' | '';
+
+export type RouterLinkCommands = any[] | string | null | undefined;
