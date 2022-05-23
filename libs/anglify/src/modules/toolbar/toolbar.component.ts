45c197be89dea83bc20e72dba7cd8b764e083908
Lucas Kuess <lucas@kuess.io>
2022-05-23 13:29:26 +0200

    feat(Toolbar): add elevation property

--- libs/anglify/src/modules/toolbar/toolbar.component.ts
+++ libs/anglify/src/modules/toolbar/toolbar.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, ContentChildren, Inject, Input, QueryList, Self } from '@angular/core';
 import { DEFAULT_TOOLBAR_SETTINGS, TOOLBAR_SETTINGS } from './toolbar-settings.token';
 import { EntireToolbarSettings } from './toolbar.interface';
+import { Elevation } from '../../composables/elevation/elevation.interface';
+import { ELEVATION } from '../../composables/elevation/elevation.provider';
+import { ElevationService } from '../../composables/elevation/elevation.service';
 import { createSettingsProvider } from '../../factories/settings.factory';
 import { BooleanLike } from '../../utils/interfaces';
 import { SlotDirective } from '../common/directives/slot/slot.directive';
@@ -10,7 +13,10 @@ import { SlotDirective } from '../common/directives/slot/slot.directive';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [createSettingsProvider<EntireToolbarSettings>('anglifyToolbarSettings', DEFAULT_TOOLBAR_SETTINGS, TOOLBAR_SETTINGS)],
+  providers: [
+    createSettingsProvider<EntireToolbarSettings>('anglifyToolbarSettings', DEFAULT_TOOLBAR_SETTINGS, TOOLBAR_SETTINGS),
+    ELEVATION,
+  ],
 })
 export class ToolbarComponent {
   @ContentChildren(SlotDirective) public readonly slots?: QueryList<SlotDirective>;
@@ -18,5 +24,19 @@ export class ToolbarComponent {
   @Input() public prominent: BooleanLike = this.settings.prominent;
   @Input() public collapse: BooleanLike = this.settings.collapse;
 
-  public constructor(@Self() @Inject('anglifyToolbarSettings') public settings: EntireToolbarSettings) {}
+  @Input()
+  public set elevation(value: Elevation) {
+    this.elevationService.elevation = value;
+  }
+
+  public get elevation() {
+    return this.elevationService.elevation;
+  }
+
+  public constructor(
+    @Self() @Inject('anglifyToolbarSettings') public settings: EntireToolbarSettings,
+    private readonly elevationService: ElevationService
+  ) {
+    this.elevation = this.settings.elevation;
+  }
 }
