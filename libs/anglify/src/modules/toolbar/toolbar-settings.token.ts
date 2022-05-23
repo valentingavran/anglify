c076c2bc16a21750c16b8f6f3e964228a76023a3
Lucas Kuess <lucas@kuess.io>
2022-05-23 13:29:26 +0200

    feat(Toolbar): add elevation property

--- libs/anglify/src/modules/toolbar/toolbar-settings.token.ts
+++ libs/anglify/src/modules/toolbar/toolbar-settings.token.ts
@@ -4,5 +4,6 @@ import { EntireToolbarSettings, ToolbarSettings } from './toolbar.interface';
 export const DEFAULT_TOOLBAR_SETTINGS: EntireToolbarSettings = {
   prominent: false,
   collapse: false,
+  elevation: 0,
 };
 export const TOOLBAR_SETTINGS = new InjectionToken<ToolbarSettings>('Toolbar Settings');
