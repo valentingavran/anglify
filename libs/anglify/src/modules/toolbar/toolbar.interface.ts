e8c61ca15ac38868c71bfa140661925fa913df5f
Lucas Kuess <lucas@kuess.io>
2022-05-23 13:29:26 +0200

    feat(Toolbar): add elevation property

--- libs/anglify/src/modules/toolbar/toolbar.interface.ts
+++ libs/anglify/src/modules/toolbar/toolbar.interface.ts
@@ -1,8 +1,10 @@
+import { Elevation } from '../../composables/elevation/elevation.interface';
 import { BooleanLike } from '../../utils/interfaces';
 
 export interface EntireToolbarSettings {
   prominent: BooleanLike;
   collapse: BooleanLike;
+  elevation: Elevation;
 }
 
 export type ToolbarSettings = Partial<EntireToolbarSettings>;
