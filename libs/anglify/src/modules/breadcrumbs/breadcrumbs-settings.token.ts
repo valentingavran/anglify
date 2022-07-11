1126075411f7be4afee4dbd69584980419031324
Valentin Gavran <valentin.gavran@icloud.com>
2022-07-11 08:45:30 +0200

    refactor(Breadcrumbs): make some item props optional
    BREAKING CHANGE: Breadcrumb matchOptions are now configured for each item separately.

--- libs/anglify/src/modules/breadcrumbs/breadcrumbs-settings.token.ts
+++ libs/anglify/src/modules/breadcrumbs/breadcrumbs-settings.token.ts
@@ -4,6 +4,5 @@ import { BreadCrumbsSettings, EntireBreadCrumbsSettings } from './breadcrumbs.in
 export const DEFAULT_BREADCRUMBS_SETTINGS: EntireBreadCrumbsSettings = {
   items: [],
   divider: '/',
-  matchOptions: { matrixParams: 'exact', queryParams: 'exact', paths: 'exact', fragment: 'exact' },
 };
 export const BREADCRUMBS_SETTINGS = new InjectionToken<BreadCrumbsSettings>('Breadcrumbs Settings');
