79be106ec65029bc371096a209d5dc1f44bc7f79
iCrawl <buechler.noel@outlook.com>
2022-07-18 13:21:44 +0200

    feat: add additional internal icons

--- libs/anglify/src/modules/icon/icon.interface.ts
+++ libs/anglify/src/modules/icon/icon.interface.ts
@@ -1,7 +1,7 @@
 import type { ComponentSize } from '../../utils/interfaces';
 
 // TODO This type must always be up-to-date (add new required internal icons here)
-export type InternalIcons = 'check' | 'chevronDown' | 'edit';
+export type InternalIcons = 'check' | 'chevronDown' | 'close' | 'closeCircle' | 'edit';
 
 export type IconSet = 'fa4' | 'fa5' | 'md' | 'mdi' | 'custom' | 'faSVG' | 'mdiSVG';
 export type InternalIconMappings = { [T in IconSet]: InternalIconSetDefinition };
