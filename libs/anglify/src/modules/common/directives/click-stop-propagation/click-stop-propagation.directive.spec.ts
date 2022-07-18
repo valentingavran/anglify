0675535f30e49f255e3271456df3eac248fb0b97
iCrawl <buechler.noel@outlook.com>
2022-07-18 13:22:19 +0200

    feat: add directive that can stop click event propagation

---
+++ libs/anglify/src/modules/common/directives/click-stop-propagation/click-stop-propagation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ClickStopPropagationDirective } from './click-stop-propagation.directive';
+
+describe('ClickStopPropagationDirective', () => {
+  it('should create an instance', () => {
+    const directive = ClickStopPropagationDirective;
+    expect(directive).toBeTruthy();
+  });
+});
