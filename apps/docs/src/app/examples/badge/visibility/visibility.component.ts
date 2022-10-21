aa56c99c98403ba81e2d278b67e881cdd51d5f3e
Lukas R <lukasrein97@gmail.com>
2022-10-21 19:22:00 +0200

    feat(Checkbox): add indeterminate state
    The Anglify checkbox can now take the value `undefined` in addition to `true` and `false`. In
    addition, the Anglify checkbox no longer has transitions. Transition CSS custom properties have
    been removed.

--- apps/docs/src/app/examples/badge/visibility/visibility.component.ts
+++ apps/docs/src/app/examples/badge/visibility/visibility.component.ts
@@ -9,5 +9,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class VisibilityComponent {
-  protected visible = true;
+  protected visible: boolean | undefined = true;
 }
