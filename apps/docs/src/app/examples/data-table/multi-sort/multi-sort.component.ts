9f99d434a37cc35a1c5f4152f410e8f43e9c81a4
Valentin Gavran <valentin.gavran@icloud.com>
2022-11-16 09:46:27 +0100

    feat(DataTable): add sortBy input property & change event

--- apps/docs/src/app/examples/data-table/multi-sort/multi-sort.component.ts
+++ apps/docs/src/app/examples/data-table/multi-sort/multi-sort.component.ts
@@ -1,3 +1,4 @@
+import type { SortSetting } from '@anglify/components';
 import { DataTableComponent, type DataTableHeader } from '@anglify/components';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { items } from '../data';
@@ -17,6 +18,13 @@ export default class MultiSortComponent {
     { text: 'Carbs (g)', value: 'carbs' },
     { text: 'Protein (g)', value: 'protein' },
     { text: 'Iron (%)', value: 'iron' },
+  ];
+
+  protected sortBy: SortSetting[] = [
+    {
+      direction: 'asc',
+      value: 'name',
+    },
   ];
 
   public items = items;
